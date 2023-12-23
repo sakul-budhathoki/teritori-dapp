3d6a46ee2e19a085c774f8588f434ca3b6953d82
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-12-23 12:45:03 +0545

    fix: app config

--- app.config.ts
+++ app.config.ts
@@ -1,45 +1,86 @@
 import { ExpoConfig } from "expo/config";
 
-const config: { expo: ExpoConfig } = {
-  expo: {
-    name: "Teritori",
-    slug: "teritori",
-    version: "1.0.0",
-    owner: "teritori",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#000000",
-    },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.teritori",
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/icon.png",
-        backgroundColor: "#FFFFFF",
+export default (): { expo: ExpoConfig } => {
+  if (process.env.isMini) {
+    return {
+      expo: {
+        name: "Teritori Mini",
+        slug: "teritori-mini",
+        version: "1.0.0",
+        owner: "teritori",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        userInterfaceStyle: "light",
+        splash: {
+          image: "./assets/splash.png",
+          resizeMode: "contain",
+          backgroundColor: "#000000",
+        },
+        updates: {
+          fallbackToCacheTimeout: 0,
+        },
+        assetBundlePatterns: ["**/*"],
+        ios: {
+          supportsTablet: true,
+          bundleIdentifier: "com.teritori.mini",
+        },
+        android: {
+          adaptiveIcon: {
+            foregroundImage: "./assets/icon.png",
+            backgroundColor: "#FFFFFF",
+          },
+          package: "com.teritori.mini",
+        },
+        web: {
+          favicon: "./assets/favicon.png",
+          bundler: "webpack",
+        },
+        extra: {
+          env: process.env,
+        },
       },
-      package: "com.teritori",
-    },
-    web: {
-      favicon: "./assets/favicon.png",
-      bundler: "webpack",
-    },
-    extra: {
-      env: process.env,
-      eas: {
-        projectId: "9ce165de-0199-478c-b3bd-8688e5ce03eb",
+    };
+  } else {
+    return {
+      expo: {
+        name: "Teritori",
+        slug: "teritori",
+        version: "1.0.0",
+        owner: "teritori",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        userInterfaceStyle: "light",
+        splash: {
+          image: "./assets/splash.png",
+          resizeMode: "contain",
+          backgroundColor: "#000000",
+        },
+        updates: {
+          fallbackToCacheTimeout: 0,
+        },
+        assetBundlePatterns: ["**/*"],
+        ios: {
+          supportsTablet: true,
+          bundleIdentifier: "com.teritori",
+        },
+        android: {
+          adaptiveIcon: {
+            foregroundImage: "./assets/icon.png",
+            backgroundColor: "#FFFFFF",
+          },
+          package: "com.teritori",
+        },
+        web: {
+          favicon: "./assets/favicon.png",
+          bundler: "webpack",
+        },
+        extra: {
+          env: process.env,
+          eas: {
+            projectId: "9ce165de-0199-478c-b3bd-8688e5ce03eb",
+          },
+        },
       },
-    },
-  },
+    };
+  }
 };
-
-export default config;
